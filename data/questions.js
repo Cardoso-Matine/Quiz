315c1835ccb1c9b9c29e9ad9ffc21709ac9bae1d
Cardoso Matine <cardoso.matine27@gmail.com>
2025-08-01 22:41:08 +0200

    Frist commit

---
+++ data/questions.js
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    id: 1,
+    question: "Qual é a capital da França?",
+    options: ["Paris", "Londres", "Roma", "Berlim"],
+    answer: "Paris"
+  },
+  {
+    id: 2,
+    question: "Qual linguagem é usada principalmente para desenvolvimento web?",
+    options: ["Python", "JavaScript", "C++", "Java"],
+    answer: "JavaScript"
+  },
+  {
+    id: 3,
+    question: "Quem criou a Microsoft?",
+    options: ["Steve Jobs", "Elon Musk", "Bill Gates", "Mark Zuckerberg"],
+    answer: "Bill Gates"
+  },
+  {
+    id: 4,
+    question: "Qual planeta é conhecido como 'Planeta Vermelho'?",
+    options: ["Marte", "Vênus", "Júpiter", "Mercúrio"],
+    answer: "Marte"
+  },
+  {
+    id: 5,
+    question: "Em que ano o homem pisou na Lua pela primeira vez?",
+    options: ["1965", "1969", "1975", "1981"],
+    answer: "1969"
+  }
+];
+
+export default questions;
